ffec0a2f5af78ce1bd597a688e4d775da1a026c7
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-07 08:24:11 +0530

    restructure

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-//require('dotenv').config()
-const express = require('express')
-//const morgan = require('morgan')
-const app = express()
-//const cors = require('cors')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-//app.use(express.static('build'))
-app.use(express.json())
-//app.use(cors())
-//app.use(morgan('tiny'))
-
-//app.get()
-//app.post()
-//app.put()
-//app.delete()
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
-
-const errorHandler = (error) => {
-  console.error(error.message)
-}
-app.use(errorHandler)
