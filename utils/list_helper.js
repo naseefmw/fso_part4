de41502303482c7d3572b286e3337335ae72ee06
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-08 09:01:24 +0530

    unit test step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,12 +3,26 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const sum = blogs.map(blog => (
-    blog.likes
-  )).reduce((sum, likes) => sum + likes, 0)
+  const sum = blogs
+    .map((blog) => blog.likes)
+    .reduce((sum, likes) => sum + likes, 0)
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes)
+  const maxLikes = Math.max(...likes)
+  const maxLikedBlog = blogs.filter((blog) => blog.likes === maxLikes)[0]
+  const result = {
+    title: maxLikedBlog.title,
+    author: maxLikedBlog.author,
+    likes: maxLikedBlog.likes,
+  }
+  return result
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog,
 }
